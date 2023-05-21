6b634522586584ac73241cd14f8bd729131ed0e4
nathan <nathanmageka6@gmail.com>
2023-05-22 00:11:21 +0300

    nathan

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+    attr_reader :title, :author, :page_count, :genre
 
+    def initialize(title)
+      @title = title
+      @author = nil
+      @page_count = nil
+      @genre = nil
+    end
+
+    def author=(author)
+      @author = author
+    end
+
+    def page_count=(page_count)
+      @page_count = page_count
+    end
+
+    def genre=(genre)
+      @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+  end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+      @brand = brand
+      @color = nil
+      @size = nil
+      @material = nil
+      @condition = nil
+    end
+
+    def cobble
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+  end
